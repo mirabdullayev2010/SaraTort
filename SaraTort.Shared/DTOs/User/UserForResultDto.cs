ece492b86df80568c41c9bb185e1e98ffe673133
mirabdullayev2010 <muhammadsodiq8177@gmail.com>
2026-07-07 17:54:11 +0500

    user model va dtolar yozildi

---
+++ SaraTort.Shared/DTOs/User/UserForResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SaraTort.Shared.DTOs.User;
+
+public class UserForResultDto
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+    public int Age { get; set; }
+}
---
+++ SaraTort.Shared/DTOs/User/UserForUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SaraTort.Shared.DTOs.User;
+
+public class UserForUpdateDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
+    public int Age { get; set; }
+}
